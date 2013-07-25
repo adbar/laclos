43b3ee350e72fdbc26475dfb038346262bff49b5
Adrien Barbaresi <adrien.barbaresi@ens-lyon.fr>
2013-07-25 17:10:57 +0200

    file types in cleaning

--- subfiles-cleaner_enbloc.py
+++ subfiles-cleaner_enbloc.py
@@ -11,7 +11,7 @@ from os import listdir
 
 
 ## TODO:
-# file types + unicode ?
+# unicode ?
 # GERMAN
 # ª
 # Bad ones: 1055792, 770828
@@ -34,7 +34,7 @@ for filename in listdir('download/temp/'): # test/
         with open('download/temp/' + filename, 'r') as f: # test/
             lines = f.read().splitlines()
 
-        # file for cleaned text in write mode 
+        # file for cleaned text in write mode
         cleanedname = filename + '_cleaned'
         try:
             outputfile = open('test/' + cleanedname, 'w')
@@ -46,9 +46,31 @@ for filename in listdir('download/temp/'): # test/
         queue = list()
         abbrv_flag = 0
         quote_flag = 0
+        upper_flag = 0
         flush = 0
         
         # main loop
+
+        # text type
+        # MicroDVD
+        if re.match(r'\{[0-9]+\}\{[0-9]+\}', lines[5]):
+            texttype = 1
+        # SubViewer
+        elif re.match(r'\[[A-Z]+\]', lines[5]):
+            texttype = 2
+        # SAMI
+        elif re.match(r'<SAMI>', lines[0]):
+            texttype = 3
+            sami_flag = 0
+        # SSA
+        elif re.match(r'﻿\[Script Info\]', lines[0]):
+            texttype = 4
+        # default
+        else:
+            texttype = 0
+
+        print (filename, lines[0], texttype, sep='\t')
+
         for line in lines:
 
             # skip empty lines
@@ -56,40 +78,83 @@ for filename in listdir('download/temp/'): # test/
             # skip subtitle source announcement
                     # skip subtitles number and times
                     if not subnumber.match(line) and not subtime.match(line) and not subannounce.search(line):
+
+                        # MicroDVD
+                        if texttype == 1:
+                            line = re.sub(r'\{[0-9]+\}\{[0-9]+\}', '', line)
+                            line = line.replace('|', ' ')
+                        # SubViewer
+                        elif texttype == 2:
+                            if re.match (r'[0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{2},[0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{2}', line) or re.match(r'\[[A-Z ]+\]', line):
+                                line = ''
+                                # continue
+                        # SAMI
+                        elif texttype == 3:
+                            if sami_flag == 0:
+                                if re.match(r'<BODY>', line):
+                                    sami_flag = 1
+                                line = ''
+                                # continue
+                            else:
+                                line = re.sub(r'</?.+?>', ' ', line)
+                                line = line.replace('&nbsp;', ' ')
+                                line = line.replace('&auml;', 'ä')
+                                line = line.replace('&uuml;', 'ü')
+                                line = line.strip(' ')
+
+                        # SSA
+                        elif texttype == 4:
+                            if not re.match(r'Dialogue: ', line):
+                                # print (line)
+                                line = ''
+                            else:
+                                line = re.sub(r'Dialogue:.+?0,,', '', line)
+                                # Dialogue: Marked=0,0:01:33.47,0:01:37.47,Default,NTP,0000,0000,0000,!Effect,
+                                # print (line)
+                                line = re.sub(r'Dialogue:.+?Effect,', '', line)
+                                line = re.sub(r'\N', ' ', line)
+                                line = re.sub(r'\{.+?\}', '', line)
+
                         # remove diverse tags
                         # str.replace is much faster
                         # if re.search(r'[<>{}]', line):
-                        if '<' in line or '{' in line or '>' in line or '}' in line or '~' in line or '[' in line or '#' in line or ']' in line:
+                        if '<' in line or '>' in line or '~' in line or '[' in line or '#' in line or ']' in line:
                             line = re.sub(r'</?.+?>', '', line)
                             line = re.sub(r'\{[0-9]+\}\{[0-9]+\}', '', line)
                             line = line.replace('o/~', '')
                             line = line.replace('~', '')
-                            line = line.replace('[br] ', ' ')
+                            line = line.replace('[br]', ' ')
                             for target in ['[', '#', ']']:
                                 line = line.replace(target, '')
 
+                        if texttype != 3 and re.match(r'[A-ZÄÖÜ ]+$', line):
+                            line = line + ' .'
+
                         # sentence buffer switch is on
-                        if sentence_buffer:
-                            sentence_buffer = sentence_buffer + ' ' + line
-                        else:
-                            sentence_buffer = line
+                        if line:
+                            if sentence_buffer:
+                                sentence_buffer = sentence_buffer + ' ' + line
+                            else:
+                                sentence_buffer = line
 
 
         # write the sentence buffer at the end
         # lookahead and lookbehind: do not split dates : r'.+?(?<![0-9])[.!?](?![0-9\.]+)'
-        for sentence in re.findall(r'[A-ZÄÖÜa-zäöü0-9"].+?(?<![0-9])[\.!?]+(?![0-9]+)', sentence_buffer):
+        for sentence in re.findall(r'[A-ZÄÖÜa-zäöü0-9"].+?(?<![0-9])[\.!\?]+(?![0-9]+)', sentence_buffer):
+
             # queue control
             # quotes
-            if len(queue) > 0 and quote_flag == 1:
-                if re.match(r'"', sentence):
-                    queue.append('"') 
-                    sentence = re.sub ('^"', '', sentence)
-                flush = 1
-            # begins with capital letter (most frequent case)
-            elif len(queue) > 0 and abbrv_flag == 0 and re.match(r'[A-ZÄÖÜ]', sentence):
-                flush = 1
-            else:
-                flush = 0
+            if queue:
+                if quote_flag == 1:
+                    if re.match(r'"', sentence):
+                        queue.append('"') 
+                        sentence = re.sub ('^"', '', sentence)
+                    flush = 1
+                # begins with capital letter (most frequent case)
+                elif abbrv_flag == 0 and re.match(r'[A-ZÄÖÜ]', sentence):
+                    flush = 1
+                else:
+                    flush = 0
 
             # flush the queue
             if flush == 1:
